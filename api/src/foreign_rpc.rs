77add0666a5c6c2fc96fba18f5fc6b931f0bd0e9
cgilliard <christopher.gilliard@gmail.com>
2019-11-06 12:56:59 -0800

    supress warning about bare trait objects

--- api/src/foreign_rpc.rs
+++ api/src/foreign_rpc.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(bare_trait_objects)]
+
 //! JSON-RPC Stub generation for the Foreign API
 
 use crate::keychain::Keychain;
