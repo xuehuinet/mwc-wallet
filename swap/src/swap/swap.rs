b66037f053a81e9810f6b90ed1ab79972addea69
Holly Casaletto <hcasaletto@Hollys-MacBook-Pro.local>
2020-06-02 15:02:03 -0700

    Checking in files that got rust formatted.

--- swap/src/swap/swap.rs
+++ swap/src/swap/swap.rs
@@ -31,8 +31,8 @@ use uuid::Uuid;
 /// Dummy wrapper for the hex-encoded serialized transaction.
 #[derive(Serialize, Deserialize)]
 pub struct TxWrapper {
-        /// hex representation of transaction
-        pub tx_hex: String,
+	/// hex representation of transaction
+	pub tx_hex: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
