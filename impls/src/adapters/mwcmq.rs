80ec2f1336c4081d34eee8dd23536bdcf1372c32
cgilliard <christopher.gilliard@gmail.com>
2020-06-05 12:34:03 -0700

    rustfmt

--- impls/src/adapters/mwcmq.rs
+++ impls/src/adapters/mwcmq.rs
@@ -89,10 +89,14 @@ impl MwcMqsChannel {
 			})?;
 
 		println!(
-			"slate [{}] for [{}] MWCs sent to [{}]",
-			slate.id.to_string(),
-			amount_to_hr_string(slate.amount, false),
-			des_address,
+			<<<<<<< HEAD
+		"slate [{}] for [{}] MWCs sent to [{}]",
+		=======
+		"slate [{}] for [{}] MWCs sent successfully to [{}]",
+		>>>>>>> rustfmt
+		slate.id.to_string(),
+		amount_to_hr_string(slate.amount, false),
+		des_address,
 		);
 
 		//expect to get slate back.
@@ -192,9 +196,15 @@ impl Publisher for MWCMQPublisher {
 			&self.secret_key,
 			&source_address,
 		)
+			<<<<<<< HEAD
 			.map_err(|e| {
 				ErrorKind::MqsGenericError(format!("Unable to build txproof from the payload, {}", e))
 			})?;
+		=======
+		.map_err(|e| {
+			ErrorKind::MqsGenericError(format!("Unable to build txproof from the payload, {}", e))
+		})?;
+		>>>>>>> rustfmt
 
 		let slate = serde_json::to_string(&slate).map_err(|e| {
 			ErrorKind::MqsGenericError(format!("Unable convert Slate to Json, {}", e))
